676b69bfb45c7762ead7833f7949cfabffa5e9d1
Mateusz <mateuszbozek077@gmail.com>
2025-01-02 13:05:57 +0100

    Feat: add_singleton.rb

---
+++ app/controllers/design_pattern/singleton.rb
@@ -0,0 +1,31 @@
+class DesignPattern::Singleton
+    
+  attr_reader :value
+
+  @instance_mutex = Mutex.new
+
+  private_class_method :new
+
+  def initialize(value)
+    @value = value
+  end
+
+  def self.instance(value)
+    return @instance if @instance
+
+    @instance_mutex.synchronize do
+      @instance ||= new(value)
+    end
+    
+    @instance        
+  end
+
+  def some_business_logic
+    p "Random function for Singleton class"
+  end
+end
+
+# def test_singleton(value)
+#   singleton = Singleton.instance(value)
+#   puts singleton.value
+# end
